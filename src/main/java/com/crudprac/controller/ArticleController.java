163eb835c9cfbb9029b2eee452a2a6539bdd52f3
bigduk88 <hangdeuk@naver.com>
2021-06-08 17:23:51 +0900

    controller 작업중

--- src/main/java/com/crudprac/controller/ArticleController.java
+++ src/main/java/com/crudprac/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package com.crudprac.controller;
 
+import com.crudprac.dto.ArticleRequestDto;
+import com.crudprac.model.Article;
 import com.crudprac.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +14,22 @@ public class ArticleController {
 
     private final ArticleRepository articleRepository;
 
+    @GetMapping("/main/aritcle/list")
+    public List<Article> getArticle() {
+        return articleRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    @GetMapping("/main/article/list/detail")
+    public Article getDetail(@PathVariable Long id) {
+        return articleRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다.")
+        );
+    }
+
+    @PostMapping("main/article/list/post")
+    public Article createArticle(@RequestBody ArticleRequestDto requestDto) {
+        Article article = new Article(requestDto);
+        articleRepository.save(article);
+        return article;
+    }
 }
